97fbeb67598234487c3e6af935045a1046b49960
Samir Mejia <samir04m@gmail.com>
2019-11-01 14:53:31 -0500

    Segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes';
+  nombre = 'rias gremory';
+
+  arreglo = [1,2,3,4,5,6,7,8];
 }
